418e8d7e34fae262a15d07fe029d63da93b2808b
Adrian Garcia Cubas <adriangarciacubas@hotmail.com>
2022-03-08 14:47:17 +0100

    Creada la pantalla de inicio, interfaz y funcionalidad implementada. Gradle modificado para lanzar la pantalla creada

---
+++ PiscisApp/app/src/main/java/com/example/piscisapp/view/MenuPasswordRepartidoresActivity.java
@@ -0,0 +1,16 @@
+package com.example.piscisapp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.piscisapp.R;
+
+public class MenuPasswordRepartidoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_password_repartidores);
+    }
+}
